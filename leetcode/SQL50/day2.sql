f8dfb33430e66baf25edd2157bb577171dad6ad6
kkbughunter <karthikeyan.aas@gmail.com>
2024-05-01 13:13:15 +0530

    30/4/

--- leetcode/SQL50/day2.sql
+++ leetcode/SQL50/day2.sql
@@ -1,3 +1,10 @@
 
--- - 
+-- - 584. Find Customer Referee
 SELECT name FROM Customer WHERE referee_id IS NULL OR referee_id != 2;
+SELECT name FROM Customer WHERE referee_id IS NULL OR referee_id <> 2;
+
+-- - 595. Big Countries
+SELECT name, population, area FROM World
+WHERE area >= 3000000 OR population >= 25000000;
+
+-- - 
