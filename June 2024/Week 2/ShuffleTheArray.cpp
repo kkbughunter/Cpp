d0960ebd40016b61ded6e2731445c62f6372a060
kkbughunter <karthikeyan.aas@gmail.com>
2024-07-02 18:46:30 +0530

    new

---
+++ June 2024/Week 2/ShuffleTheArray.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+
+	a = 10;
+	b = 20;
+
+	cout << a<<10 endl;
+
+	return 0;
+}
--- cpp_practice/Repeated-Substring-Pattern.cpp
+++ cpp_practice/Repeated-Substring-Pattern.cpp
@@ -1,12 +1,39 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
 
-int main(){
-	string s = "ababab";
-	for(int i=0; i<s.size(); i++){
-		s >>= 1;
-		cout << s << endl;
-	}
+class Solution {
+public:
+    int findPermutationDifference(std::string s, std::string t) {
+        // Sort both strings
+        std::string sorted_s = s;
+        std::string sorted_t = t;
+        std::sort(sorted_s.begin(), sorted_s.end());
+        std::sort(sorted_t.begin(), sorted_t.end());
 
-	return 0;
+        // Create a vector to store the original indices
+        std::vector<int> svec(s.size());
+        std::vector<int> tvec(t.size());
+
+        for (int i = 0; i < s.size(); ++i) {
+            svec[sorted_s[i]] = i;
+            tvec[sorted_t[i]] = i;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            sum += std::abs(svec[s[i]] - tvec[t[i]]);
+        }
+
+        return sum;
+    }
+};
+
+int main() {
+    Solution solution;
+    std::string s = "abc";
+    std::string t = "bca";
+    std::cout << "Permutation difference: " << solution.findPermutationDifference(s, t) << std::endl;
+    return 0;
 }
